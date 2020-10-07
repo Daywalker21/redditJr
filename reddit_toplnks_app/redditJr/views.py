4d5eb7c5f277bf9fb1a807b1b435bfa6f12be22a
Daywalker21 <17ucs064@lnmiit.ac.in>
2020-10-08 00:46:26 +0530

    Login Done

--- reddit_toplnks_app/redditJr/views.py
+++ reddit_toplnks_app/redditJr/views.py
@@ -12,38 +12,14 @@ client_secret = "5qI7bmlqOnPndkkQPw2TSjTmwCI"
 @login_required
 def home(request):
 	if request.user.is_authenticated:
-		# User_data.objects.create(user_name = request.user.username , subreddits = request.user.groups)
-		# print(request.user._password)
-		# reddit = praw.Reddit(client_id="nM4HdoqJHRz-Sg",
-  #                    client_secret="xdI1iiBoMegWgs84M09R9SvRX8k",
-  #                    password= request.user._password,
-  #                    user_agent="testscript by u/fakebot3",
-  #                    username=request.user.username)
-
-		# subreddit = reddit.subreddit('python')
-
-		# for s in subreddit.hot(limit = 5):
-		# 	print(s.title)
 
 		r = praw.Reddit(client_id=client_id,
               	client_secret=client_secret,
-              	user_agent="Using Django to find subreddit"
+              	redirect_uri = "http://127.0.0.1:8080" ,
+              	user_agent="Using Django to find subreddit",
               	)
 
-		# r.set_oauth_app_info(
-				
-		# 	)
 
-		subreddit = r.subreddit('python')
-		
-		for l in subreddit.hot(limit=5):
-			print(l)
+		print(request.user)
 
-		# url = r.get_authorize_url('uniqueKey', 'identity', True)
-		# access_information = r.get_access_information(request.user.password)
-		
-		# authenticated_user = r.get_me()
-		# print(authenticated_user.name, authenticated_user.link_karma)
-
-		print(request.user.get_username	)
 	return render(request,'html/index.html')
